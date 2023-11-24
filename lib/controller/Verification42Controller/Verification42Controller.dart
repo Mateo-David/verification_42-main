63eb9561c7ab21c4cad250c158b85b5769cd083d
Muneeb@1437-2020 <muneeb.shahid@launchbox.pk>
2023-11-24 12:09:01 +0500

    commit

---
+++ lib/controller/Verification42Controller/Verification42Controller.dart
@@ -0,0 +1,66 @@
+import 'package:get/get.dart';
+import 'package:verification_42/Components/StatisticsCard/StatisticsCard.dart';
+import 'package:verification_42/model/Verification42/Verification42Model.dart';
+
+class Verification42Controller extends GetxController {
+  final RxBool _isChecked = false.obs;
+
+  onCheckboxChanged() {
+    _isChecked.toggle();
+  }
+
+  RxBool first = false.obs;
+  RxBool second = false.obs;
+  RxBool third = false.obs;
+
+  void updateFirstCheckbox(bool value) {
+    first.value = value;
+  }
+
+  void updateSecondCheckBox(bool value) {
+    second.value = value;
+  }
+
+  void updateThirdCheckBox(bool value) {
+    third.value = value;
+  }
+
+  List<Verification42Model> verification42Card = [
+    Verification42Model(
+      numericValue: "\n36000",
+      text2: "Monthly\nOrder Value",
+      text1: 'PKR',
+      loadingbar: false,
+      obtained: '',
+      total: '',
+    ),
+    Verification42Model(
+        numericValue: "\n546",
+        text2: "Number \nof Orders",
+        obtained: '',
+        total: '',
+        text1: 'Units',
+        loadingbar: false),
+    Verification42Model(
+        numericValue: "",
+        text2: "Working Days",
+        text1: '',
+        obtained: '12',
+        total: '/22',
+        loadingbar: true),
+  ];
+
+  List <String> filteredResults = [];
+
+  void updateFilteredData() {
+    filteredResults.clear();
+    if (first.value) {
+      filteredResults.add(verification42Card[0] as String);
+    }  if (second.value) {
+      filteredResults.add(verification42Card[1] as String);
+    }  if (third.value) {
+      filteredResults.add(verification42Card[2] as String);
+    }
+    update(); 
+  }
+}
